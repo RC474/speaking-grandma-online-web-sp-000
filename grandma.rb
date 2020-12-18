b41a66d8b3201b554fd5274404ca86ebb21690d1
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-12-18 22:49:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,11 @@
 
 
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  if string.upcase == false 
+    return "HUH?! SPEAK UP, SONNY!"
+  else string.upcase 
+    return "NO, NOT SINCE 1938!"
+  else string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUNPKIN!"
   end
 end
